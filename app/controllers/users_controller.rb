63f561452c6e6f2c41e0445e15e5519cb78e29a9
Daniel Tobon <shiriux1@gmail.com>
2017-05-08 15:45:42 -0500

    Implementacion de login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Te has registrado correctamente"
       redirect_to @user
 
