fdaf8a912daca84732f979b827f3ca92ebc54350
Daniel Tobon <shiriux1@gmail.com>
2017-06-14 12:05:18 -0500

    Correccion errores

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      flash[:success] = "Has iniciado Sesión correctamente"
       log_in(user)
       redirect_to root_path
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "Te has registrado correctamente"
-      redirect_to @user
+      redirect_to root_path
 
     else
       render 'new'
